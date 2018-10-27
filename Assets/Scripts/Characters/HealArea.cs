02bc17c671eb5dac20c9326e2bd6c39253869f2f
LuisARodr <42625933+LuisARodr@users.noreply.github.com>
2018-10-26 21:44:09 -0700

    heals locos y project version

---
+++ Assets/Scripts/Characters/HealArea.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealArea : MonoBehaviour {
+
+    public ArrayList gOInside;
+    [SerializeField, Range(1,5)]
+    protected int maxNumberOfHeals;
+    [SerializeField, Range(.5f,10f)]
+    protected float areaRadius;
+    protected SphereCollider effectArea;
+
+    private void Start()
+    {
+        effectArea = GetComponent<SphereCollider>();
+        effectArea.radius = areaRadius;
+        gOInside = new ArrayList();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Character")
+        {
+            if (!gOInside.Contains(other.gameObject) && gOInside.Count < maxNumberOfHeals -1 )
+            {
+                gOInside.Add(other.gameObject);
+                //agregar algun efecto o algo cuando estan dentro del healArea
+            }
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Character")
+        {
+            if (!gOInside.Contains(other.gameObject))
+            {
+                gOInside.Remove(other.gameObject);
+                //quitar el efecto??
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/Characters/WhiteMage.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameCore.SystemControls;
+
+public class WhiteMage : Character3D {
+
+    [SerializeField]
+    protected HealArea healArea;
+    [SerializeField,Range(0,50)]
+    protected int healManaCost;
+
+    override protected void Start()
+    {
+        usesMana = true;
+
+        base.Start();
+    }
+
+    protected override void Attack()
+    {  
+        if (Controllers.GetButton(1, "A", 1) && RefreshMana(-healManaCost))
+        {
+            print("Curado yo xD");
+            foreach (GameObject player in healArea.gOInside)
+            {
+                //alguna manera de curar a los personajes
+                print("Curado xD");
+            }
+        }
+        else if(Controllers.GetButton(1, "A", 1) && !RefreshMana(-healManaCost))
+        {
+            //hacer algo si no hay mana??
+        }
+    }
+    
+}
