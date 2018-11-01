4bec78647f2199a64a4f9235d769b7bc22b77456
Sarigna <38147931+Sarigna@users.noreply.github.com>
2018-11-01 01:20:44 -0700

    Arquera pelona

---
+++ Assets/Scripts/Characters/Archer/Archer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameCore.SystemControls;
+using GameCore.ObjectPooler;
+
+public class Archer : Character3D {
+
+	[SerializeField]
+	Animator anim;
+	[SerializeField]
+	GameObject arrowSpawner;
+	ObjectPooler objectPooler;
+
+	AnimatorStateInfo animStateInfo;
+
+	override protected void Start(){
+		base.Start ();
+		objectPooler = ObjectPooler.Instance;
+	}
+
+	override protected void Move() {
+		if (!Controllers.GetButton (1, "A", 2)) {
+			base.Move ();
+			anim.SetFloat ("Velocity", Mathf.Abs (rb.velocity.x + rb.velocity.z));
+		}
+	}
+
+	override protected void Attack() {
+		if (Controllers.GetButton (1, "A", 1)) {
+			anim.SetBool ("Attack", true);
+		}
+		if (Controllers.GetButton (1, "A", 2)) {
+			animStateInfo = anim.GetCurrentAnimatorStateInfo (0);
+			if (animStateInfo.IsName("shoot-still")) {
+				objectPooler.GetObjectFromPool ("Arrow", arrowSpawner.transform.position, arrowSpawner.transform.rotation, null);
+			}
+			anim.SetBool ("Attack", false);
+		}
+	}
+}
---
+++ Assets/Scripts/Characters/Archer/Arrows.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameCore.ObjectPooler;
+
+public class Arrows : MonoBehaviour {
+
+	[SerializeField]
+	protected string poolTag;
+	[SerializeField]
+	float velocity;
+	[SerializeField]
+	Rigidbody rb;
+	ObjectPooler objectPooler;
+
+	void Start(){
+		objectPooler = ObjectPooler.Instance;
+		rb.velocity = transform.forward * velocity * Time.deltaTime;
+	}
+
+}
